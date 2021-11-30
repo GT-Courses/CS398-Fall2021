f9f3b63e4b842161041ca2a8e818bb20d1e9bea3
GT-Courses <95264130+GT-Courses@users.noreply.github.com>
2021-11-29 22:11:44 -0500

    Create participants.py

---
+++ code/participants.py
@@ -0,0 +1,13 @@
+/**
+ * team.py
+ *   A simple python program to print the participants from the class.
+ *   Intended as an example of updating code on GitHub.
+ */
+
+def teammembers(myName):
+    print("Name: ", myName)
+    
+
+# Function call to add your name
+# Example
+#teammembers("GT") 
