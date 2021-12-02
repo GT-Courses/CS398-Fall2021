b0d45d2307a398e6bcdbb44a3013c883a043b389
Alycia Rose <alyciarose@MacBook-Pro.local>
2021-12-02 16:32:20 -0500

    Added function call to print Alycia Rose

--- code/participants.py
+++ code/participants.py
@@ -1,8 +1,8 @@
-/**
+"""
  * team.py
  *   A simple python program to print the participants from the class.
  *   Intended as an example of updating code on GitHub.
- */
+"""
 
 def teammembers(myName):
     print("Name: ", myName)
@@ -11,3 +11,6 @@ def teammembers(myName):
 # Function call to add your name
 # Example
 #teammembers("GT") 
+
+# To print for Alycia Rose
+teammembers("Alycia Rose")
